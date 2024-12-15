120bd209bc305c2fc905ecddd947db27a1bdae69
doing1024 <doing1024@outlook.it>
2024-12-15 12:35:32 +0800

    init for git

---
+++ elpa/pangu-spacing-20221025.522/pangu-spacing-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "pangu-spacing" "20221025.522"
+  "Minor-mode to add space between Chinese and English characters."
+  ()
+  :url "https://github.com/coldnew/pangu-spacing"
+  :commit "2303013e5cd7852136f1429162fea0e1c8cb0221"
+  :revdesc "2303013e5cd7"
+  :authors '(("coldnew" . "coldnew.tw@gmail.com"))
+  :maintainers '(("coldnew" . "coldnew.tw@gmail.com")))
